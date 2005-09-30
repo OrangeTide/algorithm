a1928fe51d84ae6ab1200bf6e21ab751e4dfccd7
mish <mish>
2005-09-30 16:57:30 +0000

    Case-insensitive version of strstr. The dumbest algorithm possible.

---
+++ strcasestr.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include <stdlib.h>
+
+/*
+* case-insensitive version of strstr()
+*/
+const char *
+strcasestr ( const char *haystack, const char *needle)
+{
+    const char *h;
+    const char *n;
+
+    h = haystack;
+    n = needle;
+    if( !*n )
+        return haystack;
+    while( *haystack ) {
+        if( tolower((unsigned char) *h) == tolower((unsigned char) *n) ) {
+            h++;
+            n++;
+            if ( !*n )
+                return haystack;
+        } else {
+            h = ++haystack;
+            n = needle;
+        }
+    }
+    return NULL;
+} 
